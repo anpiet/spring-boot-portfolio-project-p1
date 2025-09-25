f7ad40f3deb00a7237b9fd7d2ec7e6f65f856d1a
Andrzej Pietkiewicz <pietan@me.com>
2025-09-25 21:34:04 +0200

    initial commit

---
+++ src/main/java/mate/academy/SpringBootPortfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package mate.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPortfolioProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPortfolioProjectApplication.class, args);
+    }
+
+}
